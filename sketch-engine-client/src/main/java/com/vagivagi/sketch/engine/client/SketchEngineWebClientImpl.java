165134b3aded3a94d9d82a6ed7dfcaec922f961f
vagivagi <hagikazu7@gmail.com>
2021-02-22 20:27:15 +0900

    Update SketchEngineWebClientImpl.java

--- sketch-engine-client/src/main/java/com/vagivagi/sketch/engine/client/SketchEngineWebClientImpl.java
+++ sketch-engine-client/src/main/java/com/vagivagi/sketch/engine/client/SketchEngineWebClientImpl.java
@@ -5,7 +5,9 @@ import com.vagivagi.sketch.engine.response.Word;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SketchEngineWebClientImpl implements SketchEngineWebClient {
     private final RestTemplate restTemplate;
@@ -16,8 +18,11 @@ public class SketchEngineWebClientImpl implements SketchEngineWebClient {
 
     @Override
     public List<Word> getThes(String lemma) {
-        String url = "/bonito/run.cgi/thes?corpname=preloaded/bnc2&lemma=" + lemma;
-        ResponseEntity<ThesResponse> response = restTemplate.getForEntity(url, ThesResponse.class);
+        String url = "/bonito/run.cgi/thes?corpname={corpname}&lemma={lemma}";
+        Map<String, String> parameters = new HashMap<>();
+        parameters.put("corpname", "preloaded/bnc2");
+        parameters.put("lemma", lemma);
+        ResponseEntity<ThesResponse> response = restTemplate.getForEntity(url, ThesResponse.class, parameters);
         return response.getBody().getWords();
     }
 }
