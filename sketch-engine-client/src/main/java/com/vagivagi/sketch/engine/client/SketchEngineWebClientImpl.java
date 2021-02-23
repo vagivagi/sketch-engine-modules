fae55919bf9f20c4c0e5e66fba16574712b45653
vagivagi <hagikazu7@gmail.com>
2021-02-23 14:58:30 +0900

    Update SketchEngineWebClientImpl.java

--- sketch-engine-client/src/main/java/com/vagivagi/sketch/engine/client/SketchEngineWebClientImpl.java
+++ sketch-engine-client/src/main/java/com/vagivagi/sketch/engine/client/SketchEngineWebClientImpl.java
@@ -1,12 +1,11 @@
 package com.vagivagi.sketch.engine.client;
 
 import com.vagivagi.sketch.engine.response.ThesaurusResponse;
-import com.vagivagi.sketch.engine.response.Word;
+import com.vagivagi.sketch.engine.response.WordSketchResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class SketchEngineWebClientImpl implements SketchEngineWebClient {
@@ -17,12 +16,23 @@ public class SketchEngineWebClientImpl implements SketchEngineWebClient {
     }
 
     @Override
-    public List<Word> getThesaurus(String lemma) {
+    public WordSketchResponse getWordSketch(String lemma) {
+        String url = "/bonito/run.cgi/wsketch?corpname={corpname}&lemma={lemma}";
+        Map<String, String> parameters = new HashMap<>();
+        parameters.put("corpname", "preloaded/bnc2");
+        parameters.put("lemma", lemma);
+        parameters.put("maxitems", "1");
+        ResponseEntity<WordSketchResponse> response = restTemplate.getForEntity(url, WordSketchResponse.class, parameters);
+        return response.getBody();
+    }
+
+    @Override
+    public ThesaurusResponse getThesaurus(String lemma) {
         String url = "/bonito/run.cgi/thes?corpname={corpname}&lemma={lemma}";
         Map<String, String> parameters = new HashMap<>();
         parameters.put("corpname", "preloaded/bnc2");
         parameters.put("lemma", lemma);
         ResponseEntity<ThesaurusResponse> response = restTemplate.getForEntity(url, ThesaurusResponse.class, parameters);
-        return response.getBody().getWords();
+        return response.getBody();
     }
 }
