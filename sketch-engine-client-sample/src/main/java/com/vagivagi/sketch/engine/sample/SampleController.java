27b253ff0a368ed4916419aea9955fb6314691f0
vagivagi <hagikazu7@gmail.com>
2021-03-01 23:56:08 +0900

    Update SampleController.java

--- sketch-engine-client-sample/src/main/java/com/vagivagi/sketch/engine/sample/SampleController.java
+++ sketch-engine-client-sample/src/main/java/com/vagivagi/sketch/engine/sample/SampleController.java
@@ -2,6 +2,7 @@ package com.vagivagi.sketch.engine.sample;
 
 import com.vagivagi.sketch.engine.client.SketchEngineWebClient;
 import com.vagivagi.sketch.engine.response.ThesaurusResponse;
+import com.vagivagi.sketch.engine.response.WordSketchResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,9 +16,14 @@ public class SampleController {
         this.sketchEngineWebClient = sketchEngineWebClient;
     }
 
-    @GetMapping("{word}")
+    @GetMapping("thesaurus/{word}")
     public ThesaurusResponse thesaurusResponse(@PathVariable String word) {
         return this.sketchEngineWebClient.getThesaurus(word);
+    }
+
+    @GetMapping("wordSketch/{word}")
+    public WordSketchResponse wordSketchResponse(@PathVariable String word) {
+        return this.sketchEngineWebClient.getWordSketch(word);
     }
 
 }
